2feaabef0e69c742a136bf53509c6e8179ce65e5
Eliasnew52 <xxeliasnew52xx@gmail.com>
2023-11-17 00:27:05 -0600

    Updt

--- app.py
+++ app.py
@@ -1,50 +1,68 @@
 import os
 import psycopg2
-
+import requests
 
 from flask import Flask, session,render_template , redirect, request , jsonify
 from flask_session import Session
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
+from tempfile import mkdtemp
+
+
+#Hash Pass for more security
+from werkzeug.security import generate_password_hash , check_password_hash
+
+#Virtual Enviroment VARS Setup
 from dotenv import load_dotenv
 
-#Cargo las Variables de Entorno
 load_dotenv()
-
-
 
 app = Flask(__name__)
 
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 
 
-#APP Config
+# Configure session to use filesystem
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-
 Session(app)
-# Conexion de la DB
+# Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-#LOGIN ROUTE AND DEFAULT
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/')
 def index():
+    #Este limpia los datos de Sesion
+    session.clear()
+    return render_template('login.html')
+
+
+
+
+# LOGIN ROUTE
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    #Login - Capto los Datos del Formulario
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+    #Si falta algun dato del Form, Redireccionar
         if not request.form.get('username') or not request.form.get('password'):
             return redirect('/')
+        
+        #Pruebo ejecutar la busqueda dentro de la db
         try:
-            #Buscamos al Usuario en la DB
             User_Query = text("SELECT * FROM users WHERE username =:username AND password =:password")
             Query_Result = db.execute(User_Query,{'username':username,'password':password})
             User = Query_Result.fetchone()
             print(User)
             if User:  
-                #Si el Usuario Existe, Guardamos la Informacion en la Session.
                 session["user_id"] = User[0]
                 session["username"] = User[1]
                 print(session["username"])
@@ -55,17 +73,19 @@ def index():
         except Exception as e:
             print(e)
     else:
-        session.clear()
-        return render_template('login.html') # LOGIN 
+       session.clear()
+       return render_template('login.html')
 
-#Register ROUTE
+
+        
 @app.route('/register',methods = ['GET','POST'])
 def register():
     
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        
+
+    
     
         if not request.form.get('username') or not request.form.get('password'):
             return "Please Add all the Data Requiered Dude omg"
@@ -81,7 +101,7 @@ def register():
                     return"Username Already Exists"
             else:
                 try:
-                    Register_Query = text("INSERT INTO users (username,password) VALUES (:username,:password")
+                    Register_Query = text("INSERT INTO users (username,password) VALUES (:username,:password)")
                     Query_Result = db.execute(Register_Query,{'username':username,'password':password})
                     db.commit()
                     return redirect('/login')
@@ -94,8 +114,3 @@ def register():
         return render_template('register.html')
 
 
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
- 
